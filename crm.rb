0e795219862d5e375c240f5ae058863176616278
Kenny Bui <kennybui@Kennys-MacBook-Air.local>
2016-10-24 17:14:57 -0400

    working on defining UPDATE

---
+++ crm.rb
@@ -0,0 +1,50 @@
+class CRM
+
+  def initialize
+
+  end
+
+  def main_menu
+
+  end
+
+  def print_main_menu
+
+  end
+
+  def call_option
+
+  end
+
+  def add_new_contact
+
+  end
+
+  def modify_existing_contact
+
+  end
+
+  def delete_contact
+
+  end
+
+  # This method should accept as an argument an array of contacts
+  # and display each contact in that array
+  def display_contacts
+
+    # HINT: Make use of this method in the display_all_contacts and search_by_attribute methods to keep your code DRY
+  end
+
+  def display_all_contacts
+    
+    # HINT: Make use of the display_contacts method to keep your code DRY
+  end
+
+  def search_by_attribute
+    
+    # HINT: Make use of the display_contacts method to keep your code DRY
+  end
+
+  # Add other methods here, if you need them.
+
+end
